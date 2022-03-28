3c95c68941be1e52d6a0744855d5f2610ad6ffd9
Luís Von Muller <luisvmuller@icloud.com>
2022-03-28 15:50:55 -0300

    [MCA-2-FIX-2] Switched to the Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 /* Custom Components */
 import MoviesTable from "./Components/MoviesTable";
 import MoviesGenres from "./Components/MoviesGenres";
